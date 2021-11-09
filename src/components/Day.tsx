d2f3b4e8f3a09e5b0797a029711616830814376c
Asen Volev <asen.volev@egt-digital.com>
2021-11-09 19:48:23 +0200

    add active day add days from prev month add days from next month

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -2,18 +2,20 @@ import { FC } from "react";
 import styled from 'styled-components'
 
 interface Props{
-    id: number
+    date: string,
+    isToday?: boolean
 }
 
-const Day : FC<Props> = ({id}) => {
+const Day : FC<Props> = ({date, isToday = false}) => {
 
-    return <DayContainer>{id}</DayContainer>;
+    return <DayContainer isActive={isToday}>{date}</DayContainer>;
 };
 
 export default Day;
 
-const DayContainer = styled.div`
+const DayContainer = styled.div<{isActive: boolean}>`
     width:30px;
     height:30px;
     text-align:center;
+    ${({isActive})=> isActive && `background-color:red;`}
 `;
--- src/components/Month.tsx
+++ src/components/Month.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import styled from 'styled-components'
 import Day from "./Day";
+
+const weekdays = ['Mo','Tu','We','Th','Fr','Sa','Su']
 
 interface Props{
 }
@@ -11,15 +13,58 @@ const Month : FC<Props> = () => {
         return new Date(year, month, 0).getDate();
     };
 
-    const daysFromMonth = Array.from({length: getDaysOfMonth(11,2021)}, (val, key) => { return <Day id={key+1}/>});
+    const getFirstWeekDayOfMonth = (month: number,year: number) : number => {
+        return new Date(year, month-1, 1).getDay();
+    }
 
-    return <MonthContainer>{daysFromMonth}</MonthContainer>;
+    const getDaysFromPrevMonth = (month:number, year:number) : Array<JSX.Element> => {
+        const daysToReturn = getFirstWeekDayOfMonth(month,year) -1;
+        const lastDateOfPrevMonth = getDaysOfMonth(month-1,year);
+        const startDate = lastDateOfPrevMonth - daysToReturn;
+        return Array.from({length:daysToReturn}, (val, key) => 
+        {
+            return <Day key={`${key+startDate}/${month}/${year}`} date={`${key+startDate}`}/>
+        });
+    }
+
+    const getDaysFromNextMonth = (month:number, year:number) : Array<JSX.Element> => {
+        const daysToReturn = 7 - (getFirstWeekDayOfMonth(month+1,year)-1);
+
+        return Array.from({length:daysToReturn}, (val, key) => 
+        {
+            return <Day key={`${key+1}/${month}/${year}`} date={`${key+1}`}/>
+        });
+    }
+
+    const today = new Date().getDate();
+    const month = new Date().getMonth()+1;
+    const year = new Date().getFullYear();
+
+    const daysOfWeek = weekdays.map((day, index) => {return <Day key={index} date={day} />});
+
+    const daysFromPrevMonth = getDaysFromPrevMonth(month, year);
+
+    const daysFromMonth = Array.from({length: getDaysOfMonth(month,year)}, (val, key) => 
+    {
+        return <Day key={`${key+1}/${month}/${year}`} date={`${key+1}`} isToday={today === key+1}/>
+    });
+
+    const daysFromNextMonth = getDaysFromNextMonth(month, year);
+
+    return (
+        <Fragment>
+            <DaysOfWeekContainer>{daysOfWeek}</DaysOfWeekContainer>
+            <MonthContainer>{[daysFromPrevMonth,daysFromMonth,daysFromNextMonth]}</MonthContainer>
+        </Fragment>);
 };
 
 export default Month;
 
-const MonthContainer = styled.div`
+const DaysOfWeekContainer = styled.div`
     display: flex;
-    flex-wrap:wrap;
     max-width:210px;
+`;
+
+const MonthContainer = styled(DaysOfWeekContainer)`
+    flex-wrap:wrap;
 `;
