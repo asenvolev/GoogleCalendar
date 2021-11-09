496fc24dab00773e8678c1a25cfca08ba3472251
Asen Volev <asen.volev@egt-digital.com>
2021-11-09 21:52:57 +0200

    implement moving months with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import styled from 'styled-components';
 import './App.css';
 import Day from './components/Day';
@@ -18,8 +18,21 @@ function App() {
 
   const daysOfWeek = weekdays.map((day, index) => {return <Day key={index} date={day} />});
 
+  const monthName = useMemo(()=> monthNames[(month-1) % 11], [month]);
+
+  const onNextMonthClick = () => setMonth(m=>m+1);
+
+  const onPrevMonthClick = () => setMonth(m=>m-1);
+
   return (
     <Calendar>
+      <MonthSettings>
+        <Heading>{monthName} {year}</Heading>
+        <MonthControls>
+          <ChangeMonthButton onClick={onPrevMonthClick}>&lt;</ChangeMonthButton>
+          <ChangeMonthButton onClick={onNextMonthClick}>&gt;</ChangeMonthButton>
+        </MonthControls>
+      </MonthSettings>
       <DaysOfWeekContainer>{daysOfWeek}</DaysOfWeekContainer>
       <Month month={month} year={year}/>
     </Calendar>
@@ -30,6 +43,29 @@ export default App;
 
 const Calendar = styled.div`
     max-width:210px;
+`;
+
+const MonthSettings = styled.div`
+    display:flex;
+    width:100%;
+`;
+
+const Heading = styled.div`
+    font-size:18px;
+`;
+
+const MonthControls = styled.div`
+    display:flex;
+    justify-content:flex-end;
+    flex-grow:1;
+`
+
+const ChangeMonthButton = styled.div`
+    display: flex;
+    background-color: blue;
+    width:30px;
+    justify-content:center;
+    border-radius:50%;
 `;
 
 const DaysOfWeekContainer = styled.div`
